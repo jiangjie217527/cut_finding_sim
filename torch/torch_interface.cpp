b1873a9a9ed9b84e52114f15ee68687fa02edd32
Radioheading <BruceLee_SJTU@sjtu.edu.cn>
2024-09-18 14:12:17 +0800

    feat(torch): pybind for API

---
+++ torch/torch_interface.cpp
@@ -0,0 +1,20 @@
+#include "torch_interface.h"
+#include "../src/entry.hpp"
+
+int MyExpandToSize(
+    float target_size,
+    torch::Tensor& viewpoint,
+    torch::Tensor& render_indices,
+    torch::Tensor& parent_indices,
+    torch::Tensor& view_matrix,
+    torch::Tensor& proj_matrix
+) {
+    return callAccelerator(
+        target_size,
+        viewpoint.data_ptr<float>(),
+        render_indices.data_ptr<int>(),
+        parent_indices.data_ptr<int>(),
+        view_matrix.data_ptr<float>(),
+        proj_matrix.data_ptr<float>()
+    );
+}
