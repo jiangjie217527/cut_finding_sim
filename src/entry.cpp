d21c339329ec53d7ef675ae13b20e5e96b8c6890
Xingyang Li <leashingyoung@gmail.com>
2024-09-19 15:38:17 +0800

    feat: read from binary file

--- src/entry.cpp
+++ src/entry.cpp
@@ -7,9 +7,55 @@ DCache dcache;
 std::vector<Task> tasks;
 std::vector<Node> nodes;
 std::vector<Box> boxes;
+std::vector<int> reorder_indices;
+std::vector<int> reversed_indices;
 
 void initStage() {
     // todo: load data from binary files
+    std::ifstream infile("reorder.bin", std::ios::binary);
+    if (!infile.is_open()) {
+        printf("Error: cannot open reorder.bin\n");
+        exit(1);
+    }
+
+    size_t reorder_size;
+    infile.read(reinterpret_cast<char*>(&reorder_size), sizeof(reorder_size));
+    reorder_indices.resize(reorder_size);
+    infile.read(reinterpret_cast<char*>(reorder_indices.data()), reorder_size * sizeof(int));
+    
+    size_t reversed_size;
+    infile.read(reinterpret_cast<char*>(&reversed_size), sizeof(reversed_size));
+    reversed_indices.resize(reversed_size);
+    infile.read(reinterpret_cast<char*>(reversed_indices.data()), reversed_size * sizeof(int));
+
+    size_t tasks_size;
+    infile.read(reinterpret_cast<char*>(&tasks_size), sizeof(tasks_size));
+    tasks.resize(tasks_size);
+    for (int i = 0; i < tasks_size; ++i) {
+        infile >> tasks[i];
+    }
+
+    size_t nodes_size;
+    infile.read(reinterpret_cast<char*>(&nodes_size), sizeof(nodes_size));
+    nodes.resize(nodes_size);
+    boxes.resize(nodes_size);
+    for (int i = 0; i < nodes_size; ++i) {
+        int parent, subtree_size, count_leaf;
+        infile.read(reinterpret_cast<char*>(&parent), sizeof(parent));
+        infile.read(reinterpret_cast<char*>(&subtree_size), sizeof(subtree_size));
+        infile.read(reinterpret_cast<char*>(&count_leaf), sizeof(count_leaf));
+        nodes[i] = {parent, subtree_size, count_leaf};
+
+        float minn[4], maxx[4];
+        infile.read(reinterpret_cast<char*>(minn), 4 * sizeof(float));
+        infile.read(reinterpret_cast<char*>(maxx), 4 * sizeof(float));
+
+        boxes[i].minn = {minn[0], minn[1], minn[2], minn[3]};
+        boxes[i].maxx = {maxx[0], maxx[1], maxx[2], maxx[3]};
+    }
+
+    infile.close();
+    
     dram.init(nodes, tasks, boxes);
 }
 
@@ -19,6 +65,8 @@ int callAccelerator(float target_size,
                      int* parentIndices,
                      const float* view_matrix,
                      const float* proj_matrix) {
+    initStage();
+    
     std::vector<int> render_indices, parent_indices;
     int cycle = 0;
     PE pes[PENum] = {
@@ -56,5 +104,11 @@ int callAccelerator(float target_size,
     }
 
     printf("Total cycles: %d\n", cycle);
+
+    for (int i = 0; i < render_indices.size(); ++i) {
+        renderIndices[i] = reversed_indices[renderIndices[i]];
+        parentIndices[i] = reversed_indices[parentIndices[i]];
+    }
+
     return render_indices.size();
 }
--- src/types.hpp
+++ src/types.hpp
@@ -14,6 +14,24 @@ struct Task {
     int task_size;
     std::vector<int> leaves;
     std::vector<int> leaf_task_ids;
+
+    std::ifstream& operator>>(std::ifstream& ifs, Task& task) {
+    ifs.read(reinterpret_cast<char*>(&task.start_id), sizeof(task.start_id));
+    ifs.read(reinterpret_cast<char*>(&task.task_size), sizeof(task.task_size));
+
+    int leaves_size;
+    ifs.read(reinterpret_cast<char*>(&leaves_size), sizeof(leaves_size));
+    task.leaves.resize(leaves_size);
+    ifs.read(reinterpret_cast<char*>(task.leaves.data()), leaves_size * sizeof(int));
+
+    int leaf_task_ids_size;
+    ifs.read(reinterpret_cast<char*>(&leaf_task_ids_size), sizeof(leaf_task_ids_size));
+    task.leaf_task_ids.resize(leaf_task_ids_size);
+    ifs.read(reinterpret_cast<char*>(task.leaf_task_ids.data()), leaf_task_ids_size * sizeof(int));
+
+    return ifs;
+}
+
 };
 
 struct Box {
