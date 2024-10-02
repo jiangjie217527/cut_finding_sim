9523cabea8a4e5494e014f06628b2dd5bb4649fd
Radioheading <BruceLee_SJTU@sjtu.edu.cn>
2024-10-02 21:03:51 +0800

    fix: spec about config

--- src/common.hpp
+++ src/common.hpp
@@ -15,16 +15,20 @@ constexpr int TaskQueueSize = 12;
 constexpr int PENum = 4;
 
 // for DCache
-constexpr int BankNum = 8;
-constexpr int BankSize = 2 * (TaskQueueSize + PENum) / BankNum;
+constexpr int BankNum = 4;
+constexpr int BankSize = 4 * (TaskQueueSize + PENum) / BankNum;
 constexpr int PortWordsPerCycle = 4;
 constexpr int PortNum = 4;
 constexpr int CacheWordsPerCycle = PortWordsPerCycle * PortNum;
-constexpr int BufferCacheSize = 2;
+constexpr int BufferCacheSize = 4;
 // for Task
 constexpr int MaxTaskSize = 32;
 constexpr int MaxSubtaskSize = 32;
-constexpr int SizeOfCacheData = MaxSubtaskSize * (sizeof(int) + sizeof(int)) + (sizeof(int) << 1);
+constexpr int SizeOfTask = sizeof(int) + sizeof(int) + sizeof(Box) + sizeof(int);
+constexpr int SizeofBox = sizeof(Point4) + sizeof(Point4);
+constexpr int SizeOfNode = sizeof(int) + sizeof(int) + sizeof(int) + sizeof(int) + sizeof(int) + sizeof(float) + sizeof(int) + sizeof(bool);
+constexpr int SizeOfSubtask = MaxSubtaskSize * (sizeof(int) + sizeof(int) + sizeof(int));
+constexpr int SizeOfCacheData = SizeOfTask + SizeOfSubtask + MaxTaskSize * SizeOfNode + MaxTaskSize * SizeofBox;
 // for Scheduler
 constexpr int MaxLeafBufferSize = 32;
 #endif // HGS_COMMON_HPP_
